348d26ed15eae2ecb22d986af9205188792ad2d1
Ivan Vuytsik <vuicik1989@gmail.com>
2022-07-08 19:17:49 +0300

    commit

--- client/src/components/post/Post.jsx
+++ client/src/components/post/Post.jsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 
 function Post({post}) {
 
-  const publicFolder = "https://blog-tsq.herokuapp.com/images/";;
+  const publicFolder = "https://blog-tsq.herokuapp.com/images/";
 
   return (
     <div className="post">
